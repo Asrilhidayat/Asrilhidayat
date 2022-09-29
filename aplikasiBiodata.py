20f005907b9b8097e2a4c61725edd7a87c56b04e
Asrilhidayat <114537062+Asrilhidayat@users.noreply.github.com>
2022-09-29 11:29:25 +0700

    Add files via upload

---
+++ aplikasiBiodata.py
@@ -0,0 +1,25 @@
+#Aplikasi Biodata buatan By Asril Hidayat
+
+# Input nama
+nama = input("masukan nama anda!")
+# output nama
+print("Nama anda adalah ; ",nama)
+
+#input kelas
+kelas = input("masukan kelas anda!")
+# output kelas
+print("Kelas anda adalah; ",kelas)
+
+# input NIM
+NIM = input("masukan NIM anda!")
+# output NIM
+print("NIM anda adalah; ",NIM)
+# input prodi
+prodi = input("masukan prodi anda!")
+# output prodi
+print("prodi anda adalah; ",prodi)
+
+# input TTL
+TTL = input("masukan TTL anda!")
+# output TTL
+print("TTL anda adalah;",TTL)
