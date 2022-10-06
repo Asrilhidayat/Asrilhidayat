43363afea5be9e3cf09db7145b126b8138746985
Asrilhidayat <114537062+Asrilhidayat@users.noreply.github.com>
2022-10-06 09:12:43 +0700

    Add files via upload

---
+++ tugassesi3.py
@@ -0,0 +1,11 @@
+# Biodata sederhana
+
+Nama = "Asril Hidayat"
+Kelas = "TI22A"
+Jurusan = "Teknik Informatika"
+Queto = "mengendarai untuk hidup dan hidup untuk mengendarai"
+
+print(Nama)
+print(Kelas)
+print(Jurusan)
+print(Queto)
